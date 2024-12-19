09a3521686f2b25be34b28c684da060f72135061
exodojaffar <25414650+exodojaffar@users.noreply.github.com>
2024-12-19 19:31:24 -0400

    feat: kheap with aux array and testing code

---
+++ heap/synthetic/sort.cu
@@ -0,0 +1,162 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <time.h>
+#include <queue>
+#include <algorithm>
+#include <functional>
+
+#include <uheap.cuh>
+#include <cmath>
+#include "util.hpp"
+
+using namespace std;
+
+__global__ void insertKernel(UHeap<int, int> *heap,
+                             int *items,
+                             int *aux_items,
+                             int arraySize,
+                             int batchSize)
+{
+
+    //    batchSize /= 3;
+    int batchNeed = arraySize / batchSize;
+    // insertion
+    for (int i = blockIdx.x; i < batchNeed; i += gridDim.x)
+    {
+        // insert items to buffer
+        heap->insertion(items + i * batchSize,
+                        aux_items + i * batchSize,
+                        batchSize, 0);
+        __syncthreads();
+    }
+}
+
+__global__ void deleteKernel(UHeap<int, int> *heap, int *items, int *aux_items, int arraySize, int batchSize)
+{
+
+    int batchNeed = arraySize / batchSize;
+    int size = 0;
+    // deletion
+    for (int i = blockIdx.x; i < batchNeed; i += gridDim.x)
+    {
+
+        // delete items from heap
+        if (heap->deleteRoot(items, aux_items, size) == true)
+        {
+            __syncthreads();
+
+            heap->deleteUpdate(0);
+        }
+        __syncthreads();
+    }
+}
+
+#define gpuErrchk(ans)                        \
+    {                                         \
+        gpuAssert((ans), __FILE__, __LINE__); \
+    }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort = true)
+{
+    if (code != cudaSuccess)
+    {
+        fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+        if (abort)
+            exit(code);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+
+    if (argc != 5)
+    {
+        cout << argv[0] << " [test type] [# keys in M] [keyType: 0:random 1:ascend 2:descend]\n";
+        return -1;
+    }
+
+    srand(time(NULL));
+
+    int testType = atoi(argv[1]);
+    int arrayNum = atoi(argv[2]);
+    int keyType = atoi(argv[3]);
+
+    struct timeval startTime;
+    struct timeval endTime;
+    double insertTime, deleteTime;
+    int batchNum;
+
+    int blockNum = 32;
+    int blockSize = 512;
+    int batchSize = 1024;
+
+    arrayNum = ((arrayNum + batchSize - 1) / batchSize) * batchSize;
+    batchNum = arrayNum / batchSize;
+
+    int *oriItems = new int[arrayNum];
+
+    for (int i = 0; i < arrayNum; ++i)
+    {
+        oriItems[i] = rand() % INT_MAX;
+    }
+
+    if (keyType == 1)
+    {
+        std::sort(oriItems, oriItems + arrayNum);
+    }
+    else if (keyType == 2)
+    {
+        std::sort(oriItems, oriItems + arrayNum, std::greater<int>());
+    }
+
+    // bitonic heap sort
+    UHeap<int, int> h_heap(batchNum, batchSize, INT_MAX, -1);
+
+    int *heapItems;
+    int *auxItems;
+    UHeap<int, int> *d_heap;
+
+    cudaMalloc((void **)&heapItems, sizeof(int) * (arrayNum));
+    cudaMemcpy(heapItems, oriItems, sizeof(int) * (arrayNum), cudaMemcpyHostToDevice);
+
+    cudaMalloc((void **)&auxItems, sizeof(int) * (arrayNum));
+    cudaMemcpy(auxItems, oriItems, sizeof(int) * (arrayNum), cudaMemcpyHostToDevice);
+
+    cudaMalloc((void **)&d_heap, sizeof(UHeap<int, int>));
+    cudaMemcpy(d_heap, &h_heap, sizeof(UHeap<int, int>), cudaMemcpyHostToDevice);
+
+    int smemSize = batchSize * 3 * sizeof(int) + batchSize * 3 * sizeof(int);
+    smemSize += (blockSize + 1) * sizeof(int) + 2 * batchSize * sizeof(int) + 2 * batchSize * sizeof(int);
+
+    // concurrent insetion
+    setTime(&startTime);
+
+    insertKernel<<<blockNum, blockSize, smemSize>>>(d_heap, heapItems, auxItems, arrayNum, batchSize);
+    gpuErrchk(cudaPeekAtLastError());
+    gpuErrchk(cudaDeviceSynchronize());
+
+    setTime(&endTime);
+    insertTime = getTime(&startTime, &endTime);
+
+    // concurrent deletion
+    setTime(&startTime);
+
+    deleteKernel<<<blockNum, blockSize, smemSize>>>(d_heap, heapItems, auxItems, arrayNum, batchSize);
+    gpuErrchk(cudaPeekAtLastError());
+    gpuErrchk(cudaDeviceSynchronize());
+
+    setTime(&endTime);
+    deleteTime = getTime(&startTime, &endTime);
+
+    printf("%s,insdel,%d,%dM,%.f,%.f,%.f\n",
+           argv[0] == std::string("./BGPQ_T") ? "BGPQ_T" : "BGPQ_B",
+           keyType, arrayNum, insertTime, deleteTime, insertTime + deleteTime);
+
+    cudaFree(heapItems);
+    cudaFree(auxItems);
+    heapItems = NULL;
+    cudaFree(d_heap);
+    d_heap = NULL;
+
+    return 0;
+}
---
+++ heap/uheaputil.cuh
@@ -0,0 +1,198 @@
+#ifndef HEAPUTIL_CUH
+#define HEAPUTIL_CUH
+
+#include <cstdint>
+
+template <typename K, typename U>
+__inline__ __device__ void batchCopy(K *dest, K *source, U *dest_aux, U *source_aux, int size, bool reset = false, K init_limits = 0)
+{
+    for (int i = threadIdx.x; i < size; i += blockDim.x)
+    {
+        dest[i] = source[i];
+        dest_aux[i] = source_aux[i];
+        if (reset)
+        {
+            source[i] = init_limits;
+            source_aux[i] = -1;
+        }
+    }
+    __syncthreads();
+}
+
+template <typename T>
+__inline__ __device__ void _swap(T &a, T &b)
+{
+    T tmp = a;
+    a = b;
+    b = tmp;
+}
+
+template <typename K, typename U>
+__inline__ __device__ void ibitonicSort(K *items, U *aux, int size)
+{
+
+    for (int k = 2; k <= size; k <<= 1)
+    {
+        for (int j = k / 2; j > 0; j >>= 1)
+        {
+            for (int i = threadIdx.x; i < size; i += blockDim.x)
+            {
+                int ixj = i ^ j;
+                if (ixj > i)
+                {
+                    if ((i & k) == 0)
+                    {
+                        if (items[i] > items[ixj])
+                        {
+                            _swap<K>(items[i], items[ixj]);
+                            _swap<U>(aux[i], aux[ixj]);
+                        }
+                    }
+                    else
+                    {
+                        if (items[i] < items[ixj])
+                        {
+                            _swap<K>(items[i], items[ixj]);
+                            _swap<U>(aux[i], aux[ixj]);
+                        }
+                    }
+                }
+                __syncthreads();
+            }
+        }
+    }
+}
+
+template <typename K, typename U>
+__inline__ __device__ void imergePath(K *aItems, K *bItems,
+                                      K *smallItems, K *largeItems,
+                                      U *aAuxItems, U *bAuxItems,
+                                      U *smallAuxItems, U *largeAuxItems,
+                                      int size, int smemOffset)
+{
+
+    extern __shared__ int s[];
+    K *tmpItems = (K *)&s[smemOffset];
+    U *tmpAuxItems = (U *)&tmpItems[2 * size];
+
+    int lengthPerThread = size * 2 / blockDim.x;
+
+    int index = threadIdx.x * lengthPerThread;
+    int aTop = (index > size) ? size : index;
+    int bTop = (index > size) ? index - size : 0;
+    int aBottom = bTop;
+
+    int offset, aI, bI;
+
+    // binary search for diagonal intersections
+    while (1)
+    {
+        offset = (aTop - aBottom) / 2;
+        aI = aTop - offset;
+        bI = bTop + offset;
+
+        if (aTop == aBottom || (bI < size && (aI == size || aItems[aI] > bItems[bI])))
+        {
+            if (aTop == aBottom || aItems[aI - 1] <= bItems[bI])
+            {
+                break;
+            }
+            else
+            {
+                aTop = aI - 1;
+                bTop = bI + 1;
+            }
+        }
+        else
+        {
+            aBottom = aI;
+        }
+    }
+
+    // start from [aI, bI], found a path with lengthPerThread
+    for (int i = lengthPerThread * threadIdx.x; i < lengthPerThread * threadIdx.x + lengthPerThread; ++i)
+    {
+        if (bI == size || (aI < size && aItems[aI] <= bItems[bI]))
+        {
+            tmpItems[i] = aItems[aI];
+            tmpAuxItems[i] = aAuxItems[aI];
+            aI++;
+        }
+        else if (aI == size || (bI < size && aItems[aI] > bItems[bI]))
+        {
+            tmpItems[i] = bItems[bI];
+            tmpAuxItems[i] = aAuxItems[bI];
+            bI++;
+        }
+    }
+    __syncthreads();
+
+    batchCopy(smallItems, tmpItems, smallAuxItems, tmpAuxItems, size);
+    batchCopy(largeItems, tmpItems + size, largeAuxItems, tmpAuxItems + size, size);
+}
+
+template <typename K>
+__inline__ __device__ void dbitonicSort(K *items, int size)
+{
+
+    for (int k = 2; k <= size; k <<= 1)
+    {
+        for (int j = k / 2; j > 0; j >>= 1)
+        {
+            for (int i = threadIdx.x; i < size; i += blockDim.x)
+            {
+                int ixj = i ^ j;
+                if (ixj > i)
+                {
+                    if ((i & k) == 0)
+                    {
+                        if (items[i] < items[ixj])
+                        {
+                            _swap<K>(items[i], items[ixj]);
+                        }
+                    }
+                    else
+                    {
+                        if (items[i] > items[ixj])
+                        {
+                            _swap<K>(items[i], items[ixj]);
+                        }
+                    }
+                }
+                __syncthreads();
+            }
+        }
+    }
+}
+
+template <typename K>
+__inline__ __device__ void dbitonicMerge(K *items, int size)
+{
+    for (int j = size / 2; j > 0; j /= 2)
+    {
+        for (int i = threadIdx.x; i < size; i += blockDim.x)
+        {
+            int ixj = i ^ j;
+            if ((ixj > i) && (items[i] < items[ixj]))
+                _swap<K>(items[i], items[ixj]);
+            __syncthreads();
+        }
+    }
+}
+
+template <typename K>
+__inline__ __device__ void ibitonicMerge(K *items, int size)
+{
+    for (int j = size / 2; j > 0; j /= 2)
+    {
+        for (int i = threadIdx.x; i < size; i += blockDim.x)
+        {
+            int ixj = i ^ j;
+            if ((ixj > i) && (items[i] > items[ixj]))
+                _swap<K>(items[i], items[ixj]);
+            __syncthreads();
+        }
+    }
+}
+
+#endif
