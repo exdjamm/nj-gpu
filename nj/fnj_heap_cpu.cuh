dad60336de67daac8fa797f76c19a4dc30372d1d
exodojaffar <25414650+exodojaffar@users.noreply.github.com>
2025-03-23 20:48:32 -0400

    fix: code to update sequencial math (test peding)

--- nj/fnj_heap_cpu.cuh
+++ nj/fnj_heap_cpu.cuh
@@ -13,7 +13,8 @@ typedef struct otu otu_t;
 
 void fnj_heap_cpu(nj_data_t data);
 
-void update_d(nj_data_t data, otu_t pos);
+void update_sum(nj_data_t data, otu_t *pairs, int pairs_size);
+void update_d(nj_data_t data, otu_t pos, otu_t *pairs);
 void resize_d(nj_data_t data, otu_t pos, int end_pos);
 
 void fnj_heap_cpu(nj_data_t data)
@@ -105,29 +106,20 @@ void fnj_heap_cpu(nj_data_t data)
             collected++;
         }
         printf("\n");
+
+        update_sum(data, pairs, pairs_size);
+
         for (size_t i = 0; i < collected; i++)
         {
-            update_d(data, pairs[i]);
-            resize_d(data, pairs[i], data.N - 1);
-            data.N -= 1;
-
-            printf("======== (%d, %d), %d\n", pairs[i].i, pairs[i].j, data.N + 1);
-
-            // AQUI FOI ALTERADO E ESSA ALTERACAO CAUSOU MUDANCA
-            for (int j = 0; j < collected; j++)
-            {
-                if (pairs[j].j == data.N)
-                {
-                    pairs[j].j = pairs[i].j;
-                }
-            }
+            update_d(data, pairs[i], pairs);
         }
 
         for (size_t i = 0; i < collected; i++)
         {
+            resize_d(data, pairs[i], data.N - 1 - i);
         }
 
-        // data.N -= pairs_size;
+        data.N -= pairs_size;
         run = data.N >= 3;
         if (!run)
             break;
@@ -136,25 +128,60 @@ void fnj_heap_cpu(nj_data_t data)
     free(pairs);
 }
 
-void update_d(nj_data_t data, otu_t pos)
+void update_sum(nj_data_t data, otu_t *pairs, int pairs_size)
+{
+    for (size_t i = 0; i < pairs_size; i++)
+    {
+        int i_pos = pairs[i].i;
+        int j_pos = pairs[i].j;
+
+        float d_ij = d_get_D_position(data, i_pos, j_pos);
+
+        data.S[i_pos] = (data.S[i_pos] + data.S[j_pos] - (data.N) * d_ij) / 2;
+    }
+}
+
+void update_d(nj_data_t data, otu_t pos, otu_t *pairs)
 {
     int i_pos = pos.i;
     int j_pos = pos.j;
 
     float d_ij = d_get_D_position(data, i_pos, j_pos);
-    data.S[i_pos] = (data.S[i_pos] + data.S[j_pos] - data.N * d_ij) / 2;
 
     float new_duk;
 
     for (int i = 0; i < data.N; i++)
     {
-        if (i == i_pos || i == j_pos)
+        int cont = 0;
+        for (int pair = 0; pair < data.N * data.p; pair++)
+            cont = (i == pairs[pair].i) || (i == pairs[pair].j);
+
+        if (cont)
             continue;
 
         new_duk = (d_get_D_position(data, i_pos, i) + d_get_D_position(data, j_pos, i) - d_ij) / 2;
 
         data.S[i] = data.S[i] + new_duk - (new_duk * 2 + d_ij);
         d_set_D_position(data, i_pos, i, new_duk);
+    }
+
+    for (size_t pair = 0; pair < data.N * data.p; pair++)
+    {
+        int pair_i = pairs[pair].i;
+        int pair_j = pairs[pair].i;
+
+        float d_ac, d_ad, d_bc, d_bd;
+        float d_cd = d_get_D_position(data, pair_i, pair_j);
+
+        d_ac = d_get_D_position(data, i_pos, pair_i);
+        d_ad = d_get_D_position(data, i_pos, pair_j);
+        d_bc = d_get_D_position(data, j_pos, pair_i);
+        d_bd = d_get_D_position(data, j_pos, pair_j);
+
+        new_duk = (d_ac + d_ad - 2 * d_ij + d_bc + d_bd - 2 * d_cd) / 4;
+
+        data.S[pair_i] = data.S[pair_i] + new_duk - (new_duk * 2 + d_ij);
+        d_set_D_position(data, i_pos, pair_i, new_duk);
     }
 }
 
