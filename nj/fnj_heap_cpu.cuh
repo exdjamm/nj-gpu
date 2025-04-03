cc2ddd23c8af2d48f05467cab4af7e5d02e66ff7
exodojaffar <25414650+exodojaffar@users.noreply.github.com>
2025-04-02 20:38:52 -0400

    feat: nova versao

--- nj/fnj_heap_cpu.cuh
+++ nj/fnj_heap_cpu.cuh
@@ -14,7 +14,7 @@ typedef struct otu otu_t;
 void fnj_heap_cpu(nj_data_t data);
 
 void update_sum(nj_data_t data, otu_t *pairs, int pairs_size);
-void update_d(nj_data_t data, otu_t pos, otu_t *pairs);
+void update_d(nj_data_t data, otu_t pos, otu_t *pairs, int pairs_size);
 void resize_d(nj_data_t data, otu_t pos, int end_pos);
 
 void fnj_heap_cpu(nj_data_t data)
@@ -111,7 +111,7 @@ void fnj_heap_cpu(nj_data_t data)
 
         for (size_t i = 0; i < collected; i++)
         {
-            update_d(data, pairs[i], pairs);
+            update_d(data, pairs[i], pairs, pairs_size);
         }
 
         for (size_t i = 0; i < collected; i++)
@@ -141,7 +141,7 @@ void update_sum(nj_data_t data, otu_t *pairs, int pairs_size)
     }
 }
 
-void update_d(nj_data_t data, otu_t pos, otu_t *pairs)
+void update_d(nj_data_t data, otu_t pos, otu_t *pairs, int pairs_size)
 {
     int i_pos = pos.i;
     int j_pos = pos.j;
@@ -153,34 +153,48 @@ void update_d(nj_data_t data, otu_t pos, otu_t *pairs)
     for (int i = 0; i < data.N; i++)
     {
         int cont = 0;
-        for (int pair = 0; pair < data.N * data.p; pair++)
+        for (int pair = 0; pair < pairs_size; pair++)
+        {
             cont = (i == pairs[pair].i) || (i == pairs[pair].j);
+            if (cont)
+                break;
+        }
 
         if (cont)
             continue;
 
         new_duk = (d_get_D_position(data, i_pos, i) + d_get_D_position(data, j_pos, i) - d_ij) / 2;
 
-        data.S[i] = data.S[i] + new_duk - (new_duk * 2 + d_ij);
+        data.S[i] = data.S[i] - new_duk - d_ij;
         d_set_D_position(data, i_pos, i, new_duk);
     }
 
-    for (size_t pair = 0; pair < data.N * data.p; pair++)
+    for (size_t pair = 0; pair < pairs_size; pair++)
     {
         int pair_i = pairs[pair].i;
-        int pair_j = pairs[pair].i;
+        int pair_j = pairs[pair].j;
 
         float d_ac, d_ad, d_bc, d_bd;
-        float d_cd = d_get_D_position(data, pair_i, pair_j);
+        float d_cd;
+
+        int cont = 0;
+        if (i_pos == pair_i)
+            cont = 1;
+
+        if (cont)
+            continue;
+
+        d_cd = d_get_D_position(data, pair_i, pair_j);
 
         d_ac = d_get_D_position(data, i_pos, pair_i);
         d_ad = d_get_D_position(data, i_pos, pair_j);
         d_bc = d_get_D_position(data, j_pos, pair_i);
         d_bd = d_get_D_position(data, j_pos, pair_j);
 
-        new_duk = (d_ac + d_ad - 2 * d_ij + d_bc + d_bd - 2 * d_cd) / 4;
+        new_duk = (d_ac + d_ad + d_bc + d_bd - 2 * d_cd - 2 * d_ij) / 4;
+        // printf("d_ij: %.2f\n", d_ij);
 
-        data.S[pair_i] = data.S[pair_i] + new_duk - (new_duk * 2 + d_ij);
+        data.S[pair_i] = data.S[pair_i] - new_duk - d_ij;
         d_set_D_position(data, i_pos, pair_i, new_duk);
     }
 }
